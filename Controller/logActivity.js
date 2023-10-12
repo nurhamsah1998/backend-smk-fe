ef28977ca19b51bb5608ef67ab6c7f0220d67998
Nurhamsah <nur.hamsah.cash@gmail.com>
2023-10-12 22:07:10 +0700

    improving log controller

--- Controller/logActivity.js
+++ Controller/logActivity.js
@@ -1,17 +1,36 @@
 import { logActivity } from "../Models/logActivity.js";
+import moment from "moment";
+import { Op } from "sequelize";
 
 export const getActivity = async (req, res) => {
   const page = parseInt(req.query.page) - 1 || 0;
   const limit = parseInt(req.query.limit) || 10;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
   const offside = limit * page;
   try {
-    const totalRows = await logActivity.count();
+    /// https://stackoverflow.com/a/43127894/18038473
+    const whereCondition = Boolean(endDate !== "null")
+      ? {
+          createdAt: {
+            [Op.between]: [
+              /// https://stackoverflow.com/a/12970385/18038473
+              moment(moment(startDate).startOf("day")).format(
+                "YYYY-MM-DD H:mm:ss"
+              ),
+              moment(moment(endDate).endOf("day")).format("YYYY-MM-DD H:mm:ss"),
+            ],
+          },
+        }
+      : {};
+    const totalRows = await logActivity.count({ where: { ...whereCondition } });
     const totalPage = Math.ceil(totalRows / limit);
     const logList = await logActivity.findAll({
+      where: { ...whereCondition },
       raw: true,
       limit,
       offset: offside,
-      order: [["createdAt", "ASC"]],
+      order: [["createdAt", "DESC"]],
     });
     const response = {
       data: logList.map((item) => ({
